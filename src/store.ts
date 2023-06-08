71fd2ced95af006ea87d5b0e68a93ebc7fc05d76
Madeline <grammaticomadeline@hotmail.fr>
2023-06-08 23:02:14 +0200

    fix(app) : modify redux

--- src/store.ts
+++ src/store.ts
@@ -1,23 +1,56 @@
 // import { configureStore } from "@reduxjs/toolkit"
-import produce from "immer"
 
-const connectUser = () => ({
-    type: "connectUser",
-    preload: {
-        connected: false
+import { createSlice } from "@reduxjs/toolkit";
+
+const UserSlice = createSlice({
+    name: "user",
+    initialState: {
+        email: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        connected: false,
+        token: "",
+        id: null
+    },
+    reducers: {
+        // { type: user/connectToggle, 
+        // payload: {
+        //  name: null, connected: false, id: 0
+        //}}
+        connectUser: (state, action) => {
+            state.connected= !state.connected
+
+        },
+        // loginUser:(state, action) => {
+        //     state.
+        // },
+        // signupuser: (state, action) => {
+
+        // },
+        // update: (state, action) => {
+
+        // }
     }
+
 })
+// const connectUser = () => ({
+//     type: "connectUser",
+//     preload: {
+//         connected: false
+//     }
+// })
 
-const userReducer = (state, action) => {
-    if (action.type === "connectUser") {
-        return produce(state, draft => {
-            draft.preload.connected = !draft.preload.connected
-        })
-    }
+// const userReducer = (state, action) => {
+//     if (action.type === "connectUser") {
+//         return produce(state, draft => {
+//             draft.preload.connected = !draft.preload.connected
+//         })
+//     }
 
-}
+// }
 
-initialState = {}
+// initialState = {}
 // const store = configureStore({
 //     reducer: {
 //         user: userReducer,
