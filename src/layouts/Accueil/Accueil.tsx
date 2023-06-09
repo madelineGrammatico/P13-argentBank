041b2d33921d53d26f43c5f8ed3ec54ba1367fd6
Madeline <grammaticomadeline@hotmail.fr>
2023-06-09 15:41:56 +0200

    fix(app) : add register

--- src/layouts/Accueil/Accueil.tsx
+++ src/layouts/Accueil/Accueil.tsx
@@ -1,5 +1,11 @@
+import { Register } from "../Register/Register";
+
 export function Accueil() {
     return (
+        <>
         <h1>accueil</h1>
+        <Register/>
+        </>
+        
     )
 }
